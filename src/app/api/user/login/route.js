7b83140ccdf960d8c9049bc3d18d5ea77b06f47d
Shashidharak89 <shashidharak334@gmail.com>
2025-08-09 10:23:14 +0530

    register

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -7,34 +7,28 @@ export async function POST(req) {
   await dbConnect();
   const { email, password } = await req.json();
 
-  if (!email || !password)
+  if (!email || !password) {
     return Response.json({ error: "All fields are required." }, { status: 400 });
+  }
 
   const user = await User.findOne({ email });
-  if (!user) return Response.json({ error: "Incorrect email." }, { status: 400 });
+  if (!user) {
+    return Response.json({ error: "Incorrect email or password." }, { status: 400 });
+  }
 
   const match = await bcrypt.compare(password, user.password);
-  if (!match) return Response.json({ error: "Incorrect password." }, { status: 400 });
-
-  const currentDate = new Date().setHours(0, 0, 0, 0);
-  const lastCheckin = user.lastcheckin?.setHours(0, 0, 0, 0);
-
-  if (lastCheckin === null || currentDate > lastCheckin) {
-    user.checkin = false;
-    await user.save();
+  if (!match) {
+    return Response.json({ error: "Incorrect email or password." }, { status: 400 });
   }
 
   const token = createToken(user._id);
 
   return Response.json({
     token,
-    email: user.email,
-    coins: user.coins,
-    checkin: user.checkin,
-    name: user.name,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt,
     id: user._id,
-    avatar:user.avatar,
+    name: user.name,
+    email: user.email,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt
   });
 }
