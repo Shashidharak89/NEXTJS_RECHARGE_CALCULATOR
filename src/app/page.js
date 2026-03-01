d07506dc3b164b12b533aafcc2021c2ec49c3114
Shashidhara K <shashidharak334@gmail.com>
2026-03-01 06:08:04 +0000

    Nextjs update

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 "use client";
 
-
+import { Suspense } from "react";
 import TokenVerifier from "./auth/TokenVerifier";
 import Data from "./components/Data";
 
-
 export default function HomePage() {
-
   return (
-    <main >
-      <TokenVerifier/>
-      <Data/>
+    <main>
+      <Suspense fallback={<div style={{padding:20}}>Loading...</div>}>
+        <TokenVerifier />
+        <Data />
+      </Suspense>
     </main>
   );
 }
